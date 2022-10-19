58e6c680702c379e4ce8414f7f091b347411d506
Johanss <johanss.zip@gmail.com>
2022-10-19 12:08:29 +0800

    Move now works!

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,21 @@
 type Heading = "N" | "W" | "S" | "E";
+type Coordinates = [x: number, y: number];
 const Compass: Array<Heading> = ["N", "E", "S", "W"];
 
-const turnLeft = (heading: Heading) => {
+const rotate = (turns: number) => (heading: Heading) => {
   const idx = Compass.indexOf(heading);
-  return Compass[(idx + 3) % 4];
+  return Compass[(idx + turns) % 4];
 };
 
-const turnRight = (heading: Heading) => {
-  const idx = Compass.indexOf(heading);
-  return Compass[(idx + 1) % 4];
+const turnLeft = rotate(3);
+const turnRight = rotate(1);
+
+const move = (heading: Heading, position: Coordinates) => {
+  const [x, y] = position;
+  if (heading === "N") return [x, y + 1];
+  if (heading === "E") return [x + 1, y];
+  if (heading === "S") return [x, y - 1];
+  if (heading === "W") return [x - 1, y];
 };
 
 test.each`
@@ -32,3 +39,19 @@ test.each`
   "When facing $original, turning right should cause us to face $expected",
   ({ original, expected }) => expect(turnRight(original)).toBe(expected)
 );
+
+test("When moving N, we should increment the Y coordinate", () => {
+  expect(move("N", [1, 1])).toEqual([1, 2]);
+});
+
+test("When moving E, we should increment the X coordinate", () => {
+  expect(move("E", [1, 1])).toEqual([2, 1]);
+});
+
+test("When moving S, we should increment the Y coordinate", () => {
+  expect(move("S", [1, 1])).toEqual([1, 0]);
+});
+
+test("When moving W, we should increment the Y coordinate", () => {
+  expect(move("W", [1, 1])).toEqual([0, 1]);
+});
