2f9870e94cc28903dc3b66cb0cb1e4259ffa3d10
Johanss <johanss.zip@gmail.com>
2022-10-19 11:46:38 +0800

    Turning right and left works now

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,14 @@
-const turnLeft = (x) => {
-  if (x === "N") return "W";
-  if (x === "W") return "S";
-  if (x === "S") return "E";
-  return "N";
+type Heading = "N" | "W" | "S" | "E";
+const Compass: Array<Heading> = ["N", "E", "S", "W"];
+
+const turnLeft = (heading: Heading) => {
+  const idx = Compass.indexOf(heading);
+  return Compass[(idx + 3) % 4];
+};
+
+const turnRight = (heading: Heading) => {
+  const idx = Compass.indexOf(heading);
+  return Compass[(idx + 1) % 4];
 };
 
 test.each`
@@ -14,4 +20,15 @@ test.each`
 `(
   "When facing $original, turning left should cause us to face $expected",
   ({ original, expected }) => expect(turnLeft(original)).toBe(expected)
+);
+
+test.each`
+  original | expected
+  ${"N"}   | ${"E"}
+  ${"E"}   | ${"S"}
+  ${"S"}   | ${"W"}
+  ${"W"}   | ${"N"}
+`(
+  "When facing $original, turning right should cause us to face $expected",
+  ({ original, expected }) => expect(turnRight(original)).toBe(expected)
 );
